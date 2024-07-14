3e964a2683768e4b3a89516c5bffdf4c0ceff282
pourya khaksari <52428214+pourya95@users.noreply.github.com>
2024-07-14 15:47:40 +0330

    default light

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,8 +164,8 @@ window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME ====================*/ 
 const themeButton = document.getElementById('theme-button')
-const darkTheme = 'light-theme'
-const iconTheme = 'uil-moon'
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
