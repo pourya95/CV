463f3d9c3cb8d8db35743c84211d946c6fbf6b6a
pourya khaksari <52428214+pourya95@users.noreply.github.com>
2024-07-15 17:49:07 +0330

    add default theme dark

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,7 +180,14 @@ if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
-}
+}else {
+    // If no theme was previously selected, default to dark theme
+    document.body.classList.add(darkTheme);
+    themeButton.classList.add(iconTheme);
+    // Save the default dark theme in local storage
+    localStorage.setItem('selected-theme', 'dark');
+    localStorage.setItem('selected-icon', 'uil-moon');
+  }
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
